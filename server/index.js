bf808acb1232916ef10e5d8dfccc5fc8139adb8f
Jordan Boles <jordan@Jordans-MacBook-Pro.local>
2019-05-21 16:34:36 -0600

    webpack config edit

--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database/index.js')
+const path = require('path');
+const db = require('../database/index.js');
 
 const app = express();
 const PORT = 3003;
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, '../public/dist')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/descriptions', (req, res) => {
   db.getDescriptions((err, result) => {
-    if(err) {
-      console.error(err)
+    if (err) {
+      console.error(err);
       res.sendStatus(400);
     } else {
       res.send(result);
     }
-  })
+  });
 });
 
-app.listen(PORT, () => {
+// app.get('/', (req, res) => {
+//   res.sendStatus(200);
+// });
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
   console.log(`listening on port ${PORT}`);
 });
-
