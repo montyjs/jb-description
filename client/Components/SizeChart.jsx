f8ea0315233566551b9402cba3eeca4d22de743a
Jordan Boles <jordan@Jordans-MacBook-Pro.local>
2019-05-21 18:18:51 -0600

    preliminary style setup

--- client/Components/SizeChart.jsx
+++ client/Components/SizeChart.jsx
@@ -11,7 +11,7 @@ const incrementer = (start, end) => {
   return array;
 };
 
-const usMens = [4, 4.5, '4.5+', 5, 5.5, 6, 6.5, '6.5+', 7, 7.5, 8, 8.5, 8.5, 9, 9.5, 10, 10.5, 10.5, 11, 11.5, 12, 12.5, 12.5, 13, 14];
+const usMens = [4, 4.5, '4.5+', 5, 5.5, 6, 6.5, '6.5+', 7, 7.5, 8, 8.5, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 12.5, 13, 14];
 
 const usWomens = [5, 5.5, '5.5+', 6, 6.5, 7, 7.5, '7.5+', 8, 8.5, 9, 9.5, 9.5, 10, 10.5, 11, 11.5, 11.5, 12, 12.5, 13, 13.5, 13.5, 14];
 
@@ -20,19 +20,19 @@ const SizeChart = () => (
     <h2>Size Chart</h2>
     <table>
       <tr>
-        <th className="headcol" />
+        <td className="headcol" />
         {incrementer(72, 97).map(num => <td className="headlong" key={`${num}size`}>{num.toString()}</td>)}
       </tr>
       <tr>
-        <th className="headcol">U.S. Men's</th>
+        <td className="headcol">U.S. Men's</td>
         {usMens.map(num => <td className="long" key={`${num}size`}>{num.toString()}</td>)}
       </tr>
       <tr>
-        <th className="headcol">U.S. Women's</th>
+        <td className="headcol">U.S. Women's</td>
         {usWomens.map(num => <td className="long" key={`${num}size`}>{num.toString()}</td>)}
       </tr>
       <tr>
-        <th className="headcol">EU</th>
+        <td className="headcol">EU</td>
         {incrementer(72, 97).map(num => <td className="long" key={`${num}size`}>{num.toString()}</td>)}
       </tr>
     </table>
